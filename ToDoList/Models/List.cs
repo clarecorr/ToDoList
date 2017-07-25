25e21f286f5569f6d3a6f7d9b91df3420add5f58
clarecorr <clare.e.corrigan@gmail.com>
2017-07-25 13:47:46 -0400

    added list and task classes

---
+++ ToDoList/Models/List.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class List
+    {
+        [Key]
+        public int ListID { get; set; }
+        [Display(Name = "List Title")]
+        public string ListTitle { get; set; }
+        [Display(Name = "Create Date")]
+        public DateTime ListCreateDate { get; set; }
+
+        public virtual ICollection<Task> Tasks { get; set; }
+    }
+}
